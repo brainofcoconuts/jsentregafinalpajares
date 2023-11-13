08456f1c880350911e6f53aa319be8678346c155
Cocopajares <cocopajares@gmail.com>
2023-11-12 21:32:16 -0500

    segunda preentrega

--- js/main.js
+++ js/main.js
@@ -1,63 +1,77 @@
+// Requerimiento 1: Estructura HTML del proyecto.
+// Requerimiento 2: Variables de JS necesarias.
+// Requerimiento 4: Objetos de JS.
+const servicios = {
+    diseñoPost: {
+        nombre: "Diseño de Post",
+        precio: 35
+    },
+    diseñoWeb: {
+        nombre: "Diseño Web",
+        precio: 400
+    },
+    manejoRedes: {
+        nombre: "Manejo de Redes",
+        precio: 100
+    }
+};
 
-alert
-pedirNombre();
-const MIN_PRICE = 400;
-let precioDiseñoPost = 35;
-let precioDiseñoWeb = 399;
-let precioManejoDeRedes = 100;
+// Requerimiento adicional: Arrays y Métodos de búsqueda y filtrado sobre el Array.
+const serviciosSeleccionados = [];
 
-function pedirNombre() {
-    let nombreIngresado = prompt("Por favor, ingresa tu nombre:");
-    alert("Hola, " + nombreIngresado + "! Bienvenido a nuestro servicio de diseño y manejo de redes.");
-}
-
-function saludar() {
-    let nombre = "Jorge Pajares"; 
-    console.log(nombre);
-}
-
+// Requerimiento 3: Funciones esenciales del proceso a simular.
+// Requerimiento 5: Salida de resultados por console.log.
 function calcularCostoTotal() {
     let costoTotal = 0;
 
-    while (costoTotal === 0) {
-        const checkboxDiseñoPost = document.getElementById("diseñoPost");
-        if (checkboxDiseñoPost.checked) {
-            costoTotal += precioDiseñoPost;
-            console.log("Diseño de Post seleccionado. Costo añadido: " + precioDiseñoPost);
-        }
-
-        const checkboxDiseñoWeb = document.getElementById("diseñoWeb");
-        if (checkboxDiseñoWeb.checked) {
-            costoTotal += precioDiseñoWeb;
-            console.log("Diseño Web seleccionado. Costo añadido: " + precioDiseñoWeb);
-        }
-
-        const checkboxManejoRedes = document.getElementById("manejoRedes");
-        if (checkboxManejoRedes.checked) {
-            costoTotal += precioManejoDeRedes;
-            console.log("Manejo de Redes seleccionado. Costo añadido: " + precioManejoDeRedes);
-        }
+    const checkboxDiseñoPost = document.getElementById("diseñoPost");
+    if (checkboxDiseñoPost.checked) {
+        costoTotal += servicios.diseñoPost.precio;
+        serviciosSeleccionados.push(servicios.diseñoPost.nombre);
     }
 
-    switch (true) {
-        case costoTotal >= 400:
-            costoTotal *= Math.floor(0.95);
-            console.log("Se aplicó un descuento del 5%.");
-            break;
-        case costoTotal < 400:
-            costoTotal *= 1;
-            console.log("Sin descuento");
-            break;
+    const checkboxDiseñoWeb = document.getElementById("diseñoWeb");
+    if (checkboxDiseñoWeb.checked) {
+        costoTotal += servicios.diseñoWeb.precio;
+        serviciosSeleccionados.push(servicios.diseñoWeb.nombre);
     }
+
+    const checkboxManejoRedes = document.getElementById("manejoRedes");
+    if (checkboxManejoRedes.checked) {
+        costoTotal += servicios.manejoRedes.precio;
+        serviciosSeleccionados.push(servicios.manejoRedes.nombre);
+    }
+
+    // Requerimiento adicional: Console.log para mostrar servicios seleccionados.
+    console.log("Servicios seleccionados:", serviciosSeleccionados);
 
     if (costoTotal > 0) {
-        if (costoTotal >= MIN_PRICE) {
-            document.getElementById("resultadoAdicional").textContent = "¡Obtuviste un 5% de descuento!";
-        } else if (costoTotal <= MIN_PRICE) {
-            document.getElementById("resultadoAdicional").textContent = "Completa $400 y obtén un descuento especial";
+        if (costoTotal >= 400) {
+            // Requerimiento 6: Descuentos adicionales y Salida en HTML.
+            const resultadoAdicionalElement = document.getElementById("resultadoAdicional");
+            resultadoAdicionalElement.textContent = `Costo total con descuento adicional: $${calcularDescuentoAdicional(costoTotal)}`;
         }
-        document.getElementById("resultado").textContent = `Costo total: $${costoTotal}`;
+
+        // Requerimiento 5: Salida de resultados por console.log.
+        console.log("Costo total:", costoTotal);
+
+        // Requerimiento 5: Salida de resultados en HTML.
+        const resultadoElement = document.getElementById("resultado");
+        resultadoElement.textContent = `Costo total: $${costoTotal}`;
     } else {
-        document.getElementById("resultado").textContent = "No has seleccionado ningún servicio.";
+        // Requerimiento 5: Salida de resultados en HTML.
+        const resultadoElement = document.getElementById("resultado");
+        resultadoElement.textContent = "No has seleccionado ningún servicio.";
+    }
+}
+
+// Requerimiento 7: Funciones y/o métodos para realizar operaciones.
+function calcularDescuentoAdicional(costoTotal) {
+    if (costoTotal >= 500) {
+        return costoTotal - 50;
+    } else if (costoTotal >= 400) {
+        return costoTotal - 30;
+    } else {
+        return costoTotal;
     }
 }
